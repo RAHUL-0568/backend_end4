beb4cc71f6c183b762436d855a056680f8ac6b65
RAHUL <rahul.210092004@gmail.com>
2025-09-09 15:22:53 +0530

    Your descriptive commit message here

---
+++ src/db/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+import { DB_name } from "../constant.js"; 
+
+const connectDB = async()=>{
+    try{
+      const connectionInstance = await mongoose.connect(
+  `${process.env.MONGODB_URL}/${DB_name}`
+);
+
+
+      console.log(`\nMongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+
+       
+
+    }
+    catch(eroor){
+        console.log("MONOGODB connection fail",eroor);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import {DB_name} from "./constant.js"
+import connectDB from "./db/index.js"
+
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB();
+
+
+
+// import express from'express'
+// const app=express()
+
+// (async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MOGODB_URL}/${DB_name}`)
+//        app.on("error",(error)=>{
+//         console.log("ERR",error);
+//        throw error 
+//           })
+
+//      app.listen(process.env.PORT,()=>{
+//         console.log(`app is listening on port ${process.env.PORT}`);
+        
+//      })
+
+//     }catch(error){
+//         console.log("ERROR",error);
+//         throw err
+        
+//     }
+// } ) ()
